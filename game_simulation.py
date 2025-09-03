6c133352e62d627e0d089886f6d390874a46b4d5
robrtlilligren-oss <robrt.lilligren@gmail.com>
2025-09-02 21:15:47 -0400

    Update game_simulation.py

--- game_simulation.py
+++ game_simulation.py
@@ -55,8 +55,9 @@ if st.button("Run Simulation"):
     # Display results
     st.subheader("Simulation Results")
     st.write(f"Probability of Passing Eval/Not Blowing Account: {win_probability:.4f}")
-    # st.write(f"Loss Probability: {loss_probability:.4f}")
+    st.write(f"Probability of Failure/Blowing Account: {loss_probability:.4f}")
     st.write(f"Average Number of Trades: {average_bets:.2f}")  # Changed label here
+    st.write(f"Difference is simulations that did not terminate in either win or loss.")
 
 
     # Plotting results
